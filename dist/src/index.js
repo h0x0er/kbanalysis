c8ba63b216829081f67a8b7afe652c68951d4ebe
h0x0er <jatink843@protonmail.com>
2022-03-08 13:59:30 +0530

    fixed issue with command

--- dist/src/index.js
+++ dist/src/index.js
@@ -8472,7 +8472,7 @@ async function createPR(client, content, path) {
     terminal(`mkdir -p ${path}`);
     terminal(`touch ${path}/action-security.yml`);
     terminal(`ls ${path}`);
-    terminal(`git remote origin --all`);
+    terminal(`git remote get-url origin --all`);
 }
 
 
